389d20faaab05861ce1400d91c9b801997e842b8
Allan Leal <allanjohnson.leal@gmail.com>
2021-06-23 11:41:12 -0300

    feat: add middleware in application

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import './database';
 import 'reflect-metadata';
-import express from "express";
+import 'express-async-errors';
+import express, { NextFunction,Response, Request  } from "express";
 import { router } from './routes';
 
 const app = express();
 app.use(express.json())
 app.use(router)
+
+//middleware
+app.use((err: Error, request: Request, response: Response, next: NextFunction)=>{
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+    return response.status(500).json({
+        status: "Error", 
+        message: "Internal Server Error"})
+})
 
 app.listen(3000, ()=>{
     console.info('Server is running (Port: 3000)')
